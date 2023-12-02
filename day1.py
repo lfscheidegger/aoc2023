8a701eca7623c166cb2057062ce5e48fc1ed06f0
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2023-12-02 09:53:03 -0800

    Day1, Day2

---
+++ day1.py
@@ -0,0 +1,61 @@
+with open("./day1", "r") as f:
+    lines = f.readlines()
+
+# part 1
+def part1():
+    sum = 0
+    for line in lines:
+        for char in line:
+            if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                left = char
+                break
+
+        for char in reversed(line):
+            if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                right = char
+                break
+
+        print(left, right)
+        val = int(f"{left}{right}")
+        sum += val
+
+    print(sum)
+
+#part 2
+def part2():
+    sum = 0
+    values = {
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    for line in lines:
+        lfinds = [(value, line.find(value)) for value in values]
+        rfinds = [(value, line.rfind(value)) for value in values]
+        left = min(filter(lambda find: find[1] != -1, lfinds), key=lambda find: find[1])
+        right = max(filter(lambda find: find[1] != -1, rfinds), key=lambda find: find[1])
+
+        value = int(f'{values[left[0]]}{values[right[0]]}')
+        sum += value
+
+        print(line, left[0], right[0], value)
+    print(sum)
+
+part2()
---
+++ day2.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+import re
+from typing import List
+
+with open("./day2", "r") as f:
+    lines = f.readlines()
+
+@dataclass
+class Game:
+    red: int
+    green: int
+    blue: int
+
+    def is_acceptable(self) -> bool:
+        return self.red <= 12 and self.green <= 13 and self.blue <= 14
+@dataclass
+class GameLine:
+    id: int
+    games: List[Game]
+
+    def is_acceptable(self) -> bool:
+        return all([game.is_acceptable() for game in self.games])
+
+    def power(self) -> int:
+        max_red = max(self.games, key=lambda game: game.red).red
+        max_green = max(self.games, key=lambda game: game.green).green
+        max_blue = max(self.games, key=lambda game: game.blue).blue
+
+        return max_red * max_green * max_blue
+
+
+def parse_game_line(line: str) -> GameLine:
+    (id, games_line) = line.split(":")
+    id = int(id.split(" ")[1].strip())
+    return GameLine(
+        id=id,
+        games=parse_games(games_line)
+    )
+
+
+PATTERN = r'(?:(\d+)\s*(blue|red|green))'
+
+
+def parse_games(games_line: str) -> List[Game]:
+    result = []
+    for single_game in games_line.split(";"):
+        single_game = single_game.strip()
+        matches = re.findall(PATTERN, single_game)
+        color_numbers = {color: int(number) for number, color in matches}
+
+        result.append(Game(
+            red=color_numbers.get("red", 0),
+            green=color_numbers.get("green", 0),
+            blue=color_numbers.get("blue", 0)
+        ))
+    return result
+
+def part1():
+    game_lines = [parse_game_line(line) for line in lines]
+    print(sum([game_line.id for game_line in game_lines if game_line.is_acceptable()]))
+
+def part2():
+    game_lines = [parse_game_line(line) for line in lines]
+    print(sum([game_line.power() for game_line in game_lines]))
+
+part2()
