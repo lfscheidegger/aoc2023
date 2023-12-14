3ebe9712c9be2144d552bc571b889285e839ea3f
Luiz Scheidegger <luiz.scheidegger@gmail.com>
2023-12-14 13:33:48 -0800

    Fixed day 13

--- day12.py
+++ day12.py
@@ -1,5 +1,7 @@
-from typing import List, Set, Tuple, Dict, Optional, Generator
+from functools import lru_cache
+from typing import List, Tuple, Optional, Generator
 import itertools
+from multiprocessing import Pool
 import re
 
 from aoc_api import get_input, submit
@@ -9,10 +11,11 @@ def get_combinations(row: str) -> int:
     left, right = row.split()
     damaged_counts = [int(x) for x in right.split(",")]
 
-    return count_valid_arrangements(left, damaged_counts)
+    return count_valid_arrangements(left, tuple(damaged_counts))
 
 
-def count_valid_arrangements(springs: str, groups: List[int]) -> int:
+@lru_cache(maxsize=10000000)
+def count_valid_arrangements(springs: str, groups: Tuple[int]) -> int:
     if len(groups) == 0 and all([x not in ['?', '#'] for x in springs]):
         return 1
     elif len(groups) == 0 and any([x == '#' for x in springs]):
@@ -50,6 +53,7 @@ def count_valid_arrangements(springs: str, groups: List[int]) -> int:
     return result
 
 
+@lru_cache(maxsize=10000000)
 def find_question_mark_span(text: str) -> Optional[Tuple[int, int]]:
     match = re.search(r'\?+', text)
     if match:
@@ -66,6 +70,7 @@ def generate_all_options(text: str, span: Optional[Tuple[int, int]]) -> Generato
             yield text[:span[0]] + ''.join(chunk) + text[span[0]+span[1]:]
 
 
+@lru_cache(maxsize=10000000)
 def find_first_contiguous_damaged(springs: str) -> Tuple[int, int]:
     """
     Returns the size of, and last index + 1, of the first contiguous group of broken springs found in the given springs
@@ -92,6 +97,18 @@ def find_first_contiguous_damaged(springs: str) -> Tuple[int, int]:
     return size, first_broken
 
 
+def expand_line(line: str, amount: int) -> str:
+    if amount == 1:
+        return line
+
+    left, right = line.split()
+
+    left = '?'.join([left.strip() for _ in range(amount)])
+    right = ','.join([right.strip() for _ in range(amount)])
+
+    return f'{left} {right}'
+
+
 def part1():
     input = get_input(12)
 
@@ -102,12 +119,38 @@ def part1():
     submit(day=12, level=1, answer=answer, really=True)
 
 
+def answer_single(line: str) -> int:
+    initial_count = get_combinations(line)
+    ratio = get_combinations(expand_line(line, 2)) // initial_count
+    return initial_count * ratio ** 4
+
+
 def part2():
     input = get_input(12)
 
-    answer = 0
+    p = Pool()
+    numbers = p.map(answer_single, input)
+    answer = sum(numbers)
+    #answer = 0
+    #for idx, line in enumerate(input):
+    #    initial_count = get_combinations(line)
+    #    ratio = get_combinations(expand_line(line, 2)) // initial_count
+    #    answer += initial_count * ratio ** 4
+    #    print(idx)
+        #print(line)
+        #for expand_count in range(1, 6):
+        #    expanded = expand_line(line, expand_count)
+        #    print(expand_count, get_combinations(expanded))
+        #print()
+        #expanded = expand_line(line, 5)
+        #print(line, '------', expanded)
+        #answer += get_combinations(expanded)
+        #print(answer)
+        #print(get_combinations(line))
+        #print(get_combinations(expanded))
 
     submit(day=12, level=2, answer=answer)
 
 
-part1()
+if __name__ == '__main__':
+    part2()
